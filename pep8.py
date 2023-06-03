0d1035f372587171f76bdb44580ef71f21443097
BioDani <dtejada@unal.edu.co>
2023-06-02 23:30:21 -0500

    estandar del pep 257 y pep8
    Signed-off-by: BioDani <dtejada@unal.edu.co>

---
+++ pep8.py
@@ -0,0 +1,46 @@
+"""Aquí hay un ejemplo que muestra cómo se verían 
+   los docstrings según las recomendaciones de PEP 257. 
+"""
+
+
+def mi_funcion(parametro1, parametro2):
+    """Breve descripción de la función.
+
+    Descripción más detallada de la función y su funcionalidad.
+
+    Args:
+        parametro1: Descripción del primer parámetro.
+        parametro2: Descripción del segundo parámetro.
+
+    Returns:
+        Valor de retorno: Descripción del valor de retorno (si corresponde).
+    """
+    # Cuerpo de la función
+    pass
+
+
+class MiClase:
+    """Breve descripción de la clase.
+
+    Descripción más detallada de la clase y su funcionalidad.
+    """
+
+    def __init__(self, parametro1, parametro2):
+        """Inicializador de la clase.
+
+        Args:
+            parametro1: Descripción del primer parámetro.
+            parametro2: Descripción del segundo parámetro.
+        """
+        # Cuerpo del inicializador
+
+    def mi_metodo(self):
+        """Breve descripción del método.
+
+        Descripción más detallada del método y su funcionalidad.
+
+        Returns:
+            Valor de retorno: Descripción del valor de retorno (si corresponde).
+        """
+        # Cuerpo del método
+        pass
