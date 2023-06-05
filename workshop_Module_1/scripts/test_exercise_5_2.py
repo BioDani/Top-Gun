1a3134b68a1971b472449cbb1ec2d413b7a1f4e3
BioDani <dtejada@unal.edu.co>
2023-06-04 19:56:02 -0500

    time_decorator
    Signed-off-by: BioDani <dtejada@unal.edu.co>

--- workshop_Module_1/scripts/test_exercise_5_2.py
+++ workshop_Module_1/scripts/test_exercise_5_2.py
@@ -1,5 +1,26 @@
+import time
+import random 
+
+def time_decorator(function):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = function(*args, **kwargs)
+        end_time = time.time() 
+        total_time = end_time - start_time
+        print(f'Total time (s):{total_time}')
+    return wrapper
+
+def generate_list( start: int, end: int, n_elements: int):
+    return [random.randint(start, end) for i in range(0, n_elements)]
+
+@time_decorator
+def sort_list( lst: list):
+    lst.sort
+    return lst
+
 def main():
-    pass
+    lst = generate_list(start=1,end=10,n_elements=10)
+    sort_list(lst)
     
 if __name__ == '__main__':
     main()
